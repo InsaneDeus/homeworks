971014a8e63197a318fa2fa5ae63cf5e616c74b6
InsaneDeus <insanedeusdesu@mail.ru>
2020-02-25 18:39:26 +0300

    Home work 1

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -7,21 +7,30 @@
 
 function averageMark(a, g, p) {
 	
-    // реализуйте здесь задание №1
-    
-	return // ваш ответ;
-}
+	let algebra = a;
+	let geography = g;
+	let physics = p;
+	
+	let averageScore = (algebra + geography + physics) / 3;
+	
+	return `Средний балл по предметам - ${averageScore}`;
+};
 
 function sayHello(userName) {
 	
-    // реализуйте здесь задание №2
+    let myName = userName;
+	let message = `Привет, мир! Меня зовут ${myName}`;
     
-	return // ваш ответ;
-}
+	return message;
+};
 
 function calculateFormula() {
     
-	// реализуйте здесь задание №3
+	let x = 2;
+	let y = 22;
+	let z = 0;
+
+	let result = x * y + 5 * z + x - 1;
     
-    return // ваш ответ;
-}
+    return `Ответ: ${result}`;
+};
