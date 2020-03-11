a34c511faef7cab6d5cd8bad70dde0636470a0ce
InsaneDeus <insanedeusdesu@mail.ru>
2020-03-11 16:16:09 +0300

    Homework 2.2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,34 @@
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+    if(animal === undefined){
+        return null;
+    };
+
+    let sound = animal.sound;
+
+    return sound;
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+
+    let sumNum = 0;
+
+   for(let i = 0; i < marks.length; i++){
+        sumNum = sumNum + Number(marks[i]);
+    };
+
+    let roundedAverage = Math.round(sumNum / marks.length);
+
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = Date.now();
+    
+    birthday = new Date(birthday).getTime();
+    let diff = now - birthday;
+    let age = diff / 31556952000;
+
+    console.log(age);
+
+    return (age >= 18);
 }
